13f17465d79b244bd54fb9e3da308fb67ff30a18
Knist-Larsen <nicklasklarsen@gmail.com>
2020-09-23 09:17:23 +0200

    Hjemme arbejde

--- Shooter_Sneak_Game/Assets/Scripts/enemyController.cs
+++ Shooter_Sneak_Game/Assets/Scripts/enemyController.cs
@@ -1,49 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
 public class enemyController : MonoBehaviour
 {
     public GameObject player;
-    public GameObject[] checkpoints;
+    public Queue<Vector3> checkpoints;
 
     bool playerSeen = false;
 
     float coolDownTime = 5f;
+    NavMeshAgent agent;
+    Queue<Vector3> pathPoints = new Queue<Vector3>();
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
+    void SetPoints(IEnumerable<Vector3> points)
+    {
+        pathPoints = new Queue<Vector3>(points);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        
 
         if (playerSeen == false)
         {
-            //int i = 0;
-            /*
-            for (int i = 0; i < checkpoints.Length;)
+            if (ShouldSetDestination())
             {
-                agent.destination = checkpoints[i].transform.position;
-                if (agent.destination == checkpoints[i].transform.position)
-                {
-                    i++;
-                }
+                agent.SetDestination(pathPoints.Dequeue());
             }
-            */
-            /*
-            while (i < checkpoints.Length)
+            if (pathPoints.Count == 0)
             {
-                agent.destination = checkpoints[i].transform.position;
-                
-                if (agent.destination == checkpoints[i].transform.position)
-                {
-                    i++;
-                }
-            }*/
+                pathPoints = checkpoints;
+            }
         }
         if (playerSeen == true)
         {
@@ -53,23 +51,24 @@ public class enemyController : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider player)
-    {/*
-        if (playerSeen == false)
-        {
-            playerSeen = true;
-        }
-
-        float time = 0;
-        while (time < coolDownTime)
-        {
-            time += Time.deltaTime;
-        }
-        playerSeen = false; */
-
+    {
         playerSeen = true;
     }
     private void OnTriggerExit(Collider other)
     {
         playerSeen = false;
+    }
+
+    private bool ShouldSetDestination()
+    {
+        if (pathPoints.Count == 0)
+        {
+            return false;
+        }
+        if (agent.hasPath == false || agent.remainingDistance < 0.5f)
+        {
+            return true;
+        }
+        return false;
     }
 }
