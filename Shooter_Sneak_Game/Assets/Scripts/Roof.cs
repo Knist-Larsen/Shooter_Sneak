0577ad3ffa12a217ed60328a04b3b6afe8d32755
villadsmunch <vesmunch@gmail.com>
2020-09-28 11:04:55 +0200

    Roof

---
+++ Shooter_Sneak_Game/Assets/Scripts/Roof.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roof : MonoBehaviour
+{
+
+    public GameObject roof = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            roof.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+
+        {
+            roof.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
