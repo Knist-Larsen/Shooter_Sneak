fe4543d7ae85ecbb04b0213538f6edc1c20cab89
Christian Vikkelsø <chri21i5@edu.nextkbh.dk>
2020-10-05 14:39:32 +0200

    Destroy enemies

---
+++ Shooter_Sneak_Game/Assets/Scripts/killEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killEnemy : MonoBehaviour
+{
+    /*
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    */
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Bullet")
+        {
+            print("Hit");
+            //animator.SetTrigger("Dead");
+            Destroy(gameObject);
+        }
+        
+    }
+}
