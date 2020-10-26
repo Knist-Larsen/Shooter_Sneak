456c091332f5f6a4216993e90ab41d354f7ca66f
Knist-Larsen <nicklasklarsen@gmail.com>
2020-10-26 13:48:21 +0100

    1

--- Shooter_Sneak_Game/Assets/Scripts/GameManager.cs
+++ Shooter_Sneak_Game/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private int life;
     private int maxLife = 3;
 
@@ -14,16 +15,33 @@ public class GameManager : MonoBehaviour
 
     
 
+=======
+    public GameObject Heart1, Heart2, Heart3;
+    [SerializeField]
+    public static int life;
+    public static bool targetDead = false;
+    public GameObject gameOverMenu;
+    public GameObject vindMenu;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         life = maxLife;
 
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         GameObject target = GameObject.FindGameObjectWithTag("Target");
 
 
+=======
+        life = 3;
+        Heart1.gameObject.SetActive(true);
+        Heart2.gameObject.SetActive(true);
+        Heart3.gameObject.SetActive(true);
+        gameOverMenu.SetActive(false);
+        vindMenu.SetActive(false);
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -46,14 +64,31 @@ public class GameManager : MonoBehaviour
     {
         if (life == 0)
         {
+<<<<<<< Updated upstream
             GameOver();
+=======
+            GameVon();
+            targetDead = false;
+            print("Target er død og level blev unloadet");
+>>>>>>> Stashed changes
         }
 
 
     }
     void GameOver()
     {
+<<<<<<< Updated upstream
         EnText.text = "Du har tabt";
+=======
+        gameOverMenu.SetActive(true);
+        pauseMenu.GameIsPaused = true;
+    }
+
+    void GameVon()
+    {
+        vindMenu.SetActive(true);
+        pauseMenu.GameIsPaused = true;
+>>>>>>> Stashed changes
     }
 
 
---
+++ Shooter_Sneak_Game/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public Button fortsætBtn;
+    public Button hoveMenuBtn;
+    public Button settingsBtn;
+
+    public int hovedmenuBuildIndex;
+    public int settingsBuildIndex;
+
+    private void Start()
+    {
+
+        fortsætBtn.onClick.AddListener(FortsætBtn);
+        hoveMenuBtn.onClick.AddListener(HovedmenuBtn);
+        settingsBtn.onClick.AddListener(SettingsBtn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) // Pauseknappen
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() // Fortsætter spillet
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() // Pauser Spillet
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    void FortsætBtn() // Fortsætknappens funktioner
+    {
+        Resume();
+    }
+
+    void HovedmenuBtn() // Hovedmenuknappens funktioner
+    {
+        SceneManager.LoadScene(hovedmenuBuildIndex);
+    }
+
+    void SettingsBtn() // Instillingerknappens funktioner
+    {
+        SceneManager.LoadScene(settingsBuildIndex);
+    }
+}
