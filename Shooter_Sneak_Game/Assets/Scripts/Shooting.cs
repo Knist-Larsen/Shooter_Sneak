93e4ef2d548896649f4619320a6440b7b990c329
Christian Vikkelsø <chri21i5@edu.nextkbh.dk>
2020-11-11 11:06:49 +0100

    Tilføjet nogle kommentarer

--- Shooter_Sneak_Game/Assets/Scripts/Shooting.cs
+++ Shooter_Sneak_Game/Assets/Scripts/Shooting.cs
@@ -15,6 +15,7 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //hvis venstre-musetast trykkes, så kaldes shoot-funktionen
         if (Input.GetButtonDown("Fire1"))
         {
             if (pauseMenu.GameIsPaused == false)
@@ -26,6 +27,7 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
+        //der instantieres et nyt bullet objekt med en rigidbody og en kraft
         GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(firepoint.forward * bulletForce, ForceMode.Impulse);
